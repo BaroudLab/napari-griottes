9b86064788e79cc3b8d04af61c75d7a7e883825e
Andrey Aristov <aaristov@pasteur.fr>
2022-04-15 02:16:03 +0200

    disable deletion of unused imports

--- src/napari_griottes1/__init__.py
+++ src/napari_griottes1/__init__.py
@@ -2,3 +2,8 @@ try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
+
+from ._reader import napari_get_reader
+from ._sample_data import make_cell_properties, make_zebrafish_data
+from ._widget import ExampleQWidget, example_magic_widget, make_graph
+from ._writer import write_multiple, write_single_image
