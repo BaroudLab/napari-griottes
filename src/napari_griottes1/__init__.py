ea99f384308e7296516185ede95a3d4da316204b
Andrey Aristov <aaristov@pasteur.fr>
2022-04-27 16:28:24 +0200

    update writer func

--- src/napari_griottes1/__init__.py
+++ src/napari_griottes1/__init__.py
@@ -6,4 +6,13 @@ except ImportError:
 from ._reader import napari_get_reader
 from ._sample_data import make_cell_properties, make_zebrafish_data
 from ._widget import ExampleQWidget, example_magic_widget, make_graph
-from ._writer import write_multiple, write_single_image
+from ._writer import save_graph
+
+__ALL__ = [
+    napari_get_reader,
+    make_cell_properties,
+    make_zebrafish_data,
+    example_magic_widget,
+    make_graph,
+    save_graph,
+]
