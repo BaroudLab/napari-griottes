244bfd47a1a8a4ed954b2cd8414f5705005f894c
Andrey Aristov <aaristov@pasteur.fr>
2023-03-09 13:38:15 +0100

    make external funtion for points

--- src/napari_griottes/_widget.py
+++ src/napari_griottes/_widget.py
@@ -50,43 +50,10 @@ def make_graph(
 ) -> napari.types.LayerDataTuple:
 
     # logger.info(f"you have selected {img_layer}, {img_layer.data.shape}")
+    savepath = None if datapath is None else datapath + ".graph.griottes"
+
     if point_layer is None:
-        logger.info("No points, generating")
-        raw_centers = (
-            griottes.analyse.cell_property_extraction.get_nuclei_properties(
-                label_layer.data, mask_channel=None
-            )
-        )
-        try:
-            centers = raw_centers.rename(
-                columns={
-                    "centroid-0": "z",
-                    "centroid-1": "y",
-                    "centroid-2": "x",
-                }
-            )
-            return [
-                (
-                    centers[["z", "y", "x"]],
-                    {"name": "Centers", "properties": centers, **POINT_PARAMS},
-                    "points",
-                ),
-            ]
-        except KeyError:
-            # logger.info("centers 2D")
-            centers = raw_centers.rename(
-                columns={
-                    "centroid-0": "y",
-                    "centroid-1": "x",
-                }
-            )
-            return [
-                (
-                    centers[["y", "x"]],
-                    {"name": "Centers", "properties": centers, **POINT_PARAMS},
-                    "points",
-                ),
-            ]
+        return make_point_layer(label_layer=label_layer)
     data = pd.DataFrame(point_layer.properties)
     repr(data.head())
     weights = thickness
